e9c29a7837c1d613a2ac5bb014299535bf5617ed
roushni33 <lovelyroushni33@gmail.com>
2025-03-16 19:41:30 +0530

    fixed CSS

--- src/Components/card.js
+++ src/Components/card.js
@@ -15,7 +15,7 @@ function Card({ id, image, info, price, name, removeTour }) {
 
             <div className="my-5 mx-[5px]">
                 <div className="">
-                    <h4 className="text-[#1faa59] text-xl"> ₹{price}</h4>
+                    <h4 className="text-[#1faa59] text-xl font-semibold"> ₹{price}</h4>
                     <h4 className="text-2xl">{name}</h4>
                 </div>
                 <div className="description">{description}
@@ -26,7 +26,7 @@ function Card({ id, image, info, price, name, removeTour }) {
             </div>
 
 
-            <button className="mt-[18px] py-[10px] px-[80px] border border-[#b4161b] cursor-pointer rounded-[10px] text-[18px] font-bold bg-[#b4161b21] hover:bg-red-700 hover:text-white transition-all duration-200 ease-in-out" onClick={() => removeTour(id)}>
+            <button className="mt-[18px] py-[10px] px-[80px] border border-[#b4161b] cursor-pointer rounded-[10px] text-[18px] font-bold bg-[#b4161b21] hover:bg-red-700 hover:text-white hover:border-2 hover:border-red-500 transition-all duration-200 ease-in-out" onClick={() => removeTour(id)}>
                 Not Interested
             </button>
         </div>
--- src/Components/tours.js
+++ src/Components/tours.js
@@ -4,7 +4,7 @@ function Tours({ tours, removeTour }) {
    return (
       <div className='flex justify-center items-center flex-col'>
          <div >
-            <h2 className='text-5xl border-4 border-dashed border-blue-600  rounded-2xl px-[5vw] py-[1vh'>Plan With Roushni</h2>
+            <h2 className='text-5xl border-4 border-dashed border-blue-600  rounded-xl px-[2vw] py-[1vh] '>Plan With Roushni</h2>
          </div>
 
          <div className='flex items-center justify-center flex-wrap max-w-[1600px] w-full mx-auto  '>
